aacbe038374523685fbd7a42e1c80d7364228b39
João Faria <joao.faria@astro.up.pt>
2021-09-22 12:07:56 +0100

    package

---
+++ setup.py
@@ -0,0 +1,19 @@
+""" vera """
+
+from setuptools import setup
+
+setup(
+    name='verapy',
+    version='0.0.1',
+    description='velocidades radiais',
+    url='https://github.com/j-faria/vera',
+    author='João Faria',
+    author_email='joao.faria@astro.up.pt',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['vera'],
+    zip_safe=False,
+)
