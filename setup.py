d4cff0b08cbaa56ecea8d8be5f8e9cc2d8ce146d
João Faria <joao.faria@astro.up.pt>
2021-10-27 16:36:05 +0100

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='verapy',
-    version='0.0.3',
+    version='0.0.4',
     description='velocidades radiais',
     url='https://github.com/j-faria/vera',
     author='João Faria',
